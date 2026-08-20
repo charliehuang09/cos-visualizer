5c6fb94e4507931a8a2cd5c76eb3aa3846bd20c7
Charlie Huang <charliehuang09@gmail.com>
2026-08-19 20:54:49 -0700

    Ensure deployment directory exists

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 npm run build
+ssh root@dev-orin 'mkdir -p /data/www/cos-visualizer' && \
 rsync -avz --delete dist/ root@dev-orin:/data/www/cos-visualizer
 rsync -avz nginx.conf root@dev-orin:/etc/nginx/nginx.conf
 ssh root@dev-orin 'nginx -t && systemctl reload nginx'
